9683a6bddc745b86c648892cc53b9ed248406210
Leandro Andrade <leandrade666@gmail.com>
2024-02-26 16:41:34 -0300

    Feita versão do Programa REceber Pedido com Orientação a objetos

---
+++ REceberPedido/Pedido.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceberPedido
+{
+    class Pedido
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public  double CalcularValorTotal()
+        {
+            double ValorTotal = 0;
+
+
+            if (Quantidade <= 10)
+            {
+                ValorTotal = (Preco * Quantidade);
+            }
+            else if (Quantidade > 10 && Quantidade <= 20)
+            {
+                ValorTotal = Quantidade * Preco - (Quantidade * Preco * 0.1);
+            }
+            else if (Quantidade > 20 && Quantidade <= 50)
+            {
+                ValorTotal = Quantidade * Preco - (Quantidade * Preco * 0.20);
+            }
+            else
+            {
+                ValorTotal = Quantidade * Preco - (Quantidade * Preco * 0.25);
+
+
+            }
+            return ValorTotal;
+        }
+
+    }
+}
--- REceberPedido/Program.cs
+++ REceberPedido/Program.cs
@@ -1,41 +1,32 @@
 ﻿using System;
 
-namespace REceberPedido
+namespace ReceberPedido
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Pedido Pedido = new Pedido();
+
             Console.WriteLine("\nInforme os dados do seu Pedido:");
             Console.WriteLine("\nInforme  o nome do Prduto:");
-            string Nome = Console.ReadLine();
+            Pedido.Nome = Console.ReadLine();
             Console.WriteLine("\nInforme  a quantidade do Prduto:");
-            int Quantidade = int.Parse(Console.ReadLine());
+            Pedido.Quantidade = int.Parse(Console.ReadLine());
             Console.WriteLine("\nInforme  o preço do Prduto:");
-            double Preco = double.Parse(Console.ReadLine());
-            double ValorTotal = 0;
-
-            if (Quantidade <= 10)
-            {
-                ValorTotal = (Preco * Quantidade);
-            }
-            else if (Quantidade > 10 && Quantidade <= 20) 
-            {
-                ValorTotal = Quantidade * Preco - (Quantidade * Preco * 0.1 );
-            }
-            else if(Quantidade >20 && Quantidade <=50)
-            {
-                ValorTotal = Quantidade * Preco - (Quantidade * Preco * 0.20);
-            }
-            else
-            {
-                ValorTotal = Quantidade * Preco - (Quantidade * Preco * 0.25);
+            Pedido.Preco = double.Parse(Console.ReadLine());
+            double ValorTotal = Pedido.CalcularValorTotal();
+            Console.WriteLine($"\n O none do produto é: {Pedido.Nome} e o Valor total dp seu pedido é R$ : {ValorTotal.ToString("F2")}");
 
 
-            }
-            Console.WriteLine($"\n O none do produto é: {Nome} e o Valor total dp seu pedido é R$ : {ValorTotal.ToString("F2")}");
+
+
 
 
         }
+
+
     }
 }
+
